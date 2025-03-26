46f961964c78ef1f2a2321b85e00ea52894864de
Voxxyfy <voxxyfy@gmail.com>
2025-03-27 00:08:18 +0530

    Add configuration with environment variables

---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+// Configuration for different environments
+export const SERVER_URL = process.env.VOXXYFY_API_URL || "http://localhost:3000/api";
+export const WEB_APP_URL = process.env.VOXXYFY_WEB_URL || "http://localhost:3000";
